1af0c3318866968433ca3679d838658c788e646d
Furio14 <julio.martinlo@alumnos.upm.es>
2025-10-18 12:04:48 +0800

    few changes on simulationb arrivals

--- src/GeneradorAeronaves.py
+++ src/GeneradorAeronaves.py
@@ -1,20 +1,38 @@
 import random
 from Aeronaves import *
 
+horasAeronave = f"{0:02d}"
+minutosAeronave = f"{0:02d}"
+comienzoSimulacion = False
+
 #Ciudades
 ciudades = ["Madrid", "Nueva York", "Milan", "Tokyo", "Beijing", "Shanghai", "Londres"]
 vuelos = ["IB","BA","AA","V","AV","LX","DL"]
 
 def generador():
+    global minutosAeronave
+    global horasAeronave
+    global comienzoSimulacion
     letra = random.choice("ABCDEFGHIJLKMNOPQRSTUVWXYZ") # genera letra random
     hora = random.randint(0,24)
-    minuto = random.randint(0,59)
+    minutoSalida = random.randint(0,59)
+    minutoLlegada = random.randint(0,59)
     id = f"{letra}{random.randint(0,999)}" # genera un id para un avión aleatorio
     idVuelo = f"{random.choice(vuelos)}{random.randint(0,999)}" # genera un id para un avión aleatorio
     origen,destino = random.sample(ciudades,2) # genera 2 ciudades random
-    horaSalida = f"{hora:02d}:{minuto:02d}" # genera hora random
-    duracion = random.randint(1,10)
-    horaLlegada = f"{(hora + duracion)%24:02d}:{minuto:02d}"
+    duracion = random.randint(1,5)
+    if not comienzoSimulacion:
+        horaSalida = f"{horasAeronave}:{minutosAeronave}"
+        horaLlegada = f"{(hora + duracion)%24:02d}:{minutoLlegada:02d}"
+        horasAeronave = hora + duracion
+        minutosAeronave = minutoLlegada + duracion
+        comienzoSimulacion = True
+    else:
+            if (minutosAeronave + duracion) > 59:
+                horasAeronave += 1
+                minutosAeronave = random.randint(0,4)
+            horaSalida = f"{horasAeronave - duracion:02d}:{minutoSalida:02d}"
+            horaLlegada = f"{(horasAeronave)%24:02d}:{minutosAeronave + duracion:02d}"
+            minutosAeronave = minutosAeronave + duracion
+            
     return Aeronave(id,idVuelo,origen,destino,horaSalida,horaLlegada)
-
-
