5d15266ab4f48815bc3e89b24da8c1d6da65b7fb
Furio14 <julio.martinlo@alumnos.upm.es>
2025-11-15 15:17:02 +0800

    turnos implementados

--- src/Main.py
+++ src/Main.py
@@ -7,6 +7,11 @@ from GeneradorAeronaves import *
 from Aeronaves import * 
 from FactoresExternos import *
 
+def procesos(evento,anuncio,parking,pistaAterrizaje,pistaDespegue,colaAterrizajes,colaEstacionados,colaSalidas,colaDespegues,estadoClima,retraso):
+    yield evento.timeout(retraso)
+    evento.process(torreDeControl(evento,anuncio,parking,pistaAterrizaje,pistaDespegue,colaAterrizajes,colaEstacionados,colaSalidas,colaDespegues,estadoClima))
+    evento.process(logicaClima(evento,estadoClima))
+
 def main():
     log = "../log.csv"
     #si log.txt tiene algo de información, cuando lo ejecutas se reinicia la info
@@ -27,16 +32,31 @@ def main():
         'clima':'Soleado',
         'retraso': 0.0
     }
-        horas = input("Cuantas horas quieres simular? ")
+        horas = input("Cuantas horas quieres simular? : ")
         hora = int(horas) * 60
+        turnos = input("En que turo quieres comenzar? [Madrugada,Mañana,Tarde,Noche] : ")
+        turnoNoche = ["Noche","noche"]
+        turnoTarde = ["Tarde","tarde"]
+        turnoMañana = ["Mañana","mañana"]
+        turnoMadrugada = ["Madrugada","madrugada"]
+        if turnos in turnoMadrugada:
+            retraso = 0
+        elif turnos in turnoMañana:
+            retraso = 360
+        elif turnos in turnoTarde:
+            retraso = 720
+        elif turnos in turnoNoche:
+            retraso = 1080
+        else: 
+            print("Pon un turno correcto")
+
         if hora <= 0:
             print("El numero de horas tiene que ser mayor que 0")
-        evento.process(torreDeControl(evento,anuncio,parking,pistaAterrizaje,pistaDespegue,colaAterrizajes,colaEstacionados,colaSalidas,colaDespegues,estadoClima))
-        evento.process(logicaClima(evento,estadoClima))
+        evento.process(procesos(evento,anuncio,parking,pistaAterrizaje,pistaDespegue,colaAterrizajes,colaEstacionados,colaSalidas,colaDespegues,estadoClima,retraso))
         evento.run(until=hora)
         print("------Resultados Finales de la Simulación------")
         print("Total Aeronaves Simuladas: ",Aeronave.totalAeronaves)
-        print("Total Pasajeros : ",Aeronave.totalPasajeros)
+        print("Total Pasajeros: ",Aeronave.totalPasajeros)
 
     except ValueError:
         print("No es un número valido")
